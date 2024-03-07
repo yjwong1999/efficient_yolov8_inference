e14bb3a601373c3f3b0a61907ac60657b406b60c
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2024-03-07 17:42:58 +0800

    Update single_detect.py

--- single_detect.py
+++ single_detect.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
 import cv2
-import numpy as np
 import pafy
 import concurrent.futures
-
-from collections import defaultdict
 
 import argparse
 
@@ -60,46 +57,40 @@ def predict(chosen_model, img, classes=[], conf=0.5):
    #resiz the image to 640x480
    img = cv2.resize(img, (resize_width, resize_height))
    if classes:
-       results = chosen_model.track(img, classes=classes, conf=conf, save_txt=False, persist=True)
+       results = chosen_model.predict(img, classes=classes, conf=conf, save_txt=False)
    else:
-       results = chosen_model.track(img, conf=conf, save_txt=False, persist=True)
+       results = chosen_model.predict(img, conf=conf, save_txt=False)
 
    return results
 
 
 # predict and detect
-def predict_and_detect(chosen_model, track_history, img, classes=[], conf=0.5):
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5):
    # resiz the image to 640x480
    img = cv2.resize(img, (resize_width, resize_height))
    results = predict(chosen_model, img, classes, conf=conf)
 
-   # Get the boxes and track IDs
-   boxes = results[0].boxes.xywh.cpu()
-   try:
-      track_ids = results[0].boxes.id.int().cpu().tolist()
-   except:
-      return img, results
-
-   # visualize
-   annotated_frame = results[0].plot()
-   for box, track_id in zip(boxes, track_ids):
-      x, y, w, h = box
-      track = track_history[track_id]
-      track.append((float(x), float(y)))  # x, y center point
-      if len(track) > 30:  # retain 90 tracks for 90 frames
-         track.pop(0)
-
-      # Draw the tracking lines
-      # points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-      # cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
-
-
-   return annotated_frame, results
+   for result in results:
+       for box in result.boxes:
+           #if lable is person make the box greeen and print confidence level on the box in huge font
+           if result.names[int(box.cls[0])] == "person":
+               cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                         (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (0, 255, 0), 2)
+               cv2.putText(img, f"{result.names[int(box.cls[0])]} {box.conf[0]:.2f}",
+                       (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                       cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+           else:
+               cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                         (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (0, 0, 255), 2)
+               cv2.putText(img, f"{result.names[int(box.cls[0])]} {box.conf[0]:.2f}",
+                       (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                       cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+   return img, results
 
 
 # process frame
-def process_frame(track_history, frame):
-   result_frame, _ = predict_and_detect(chosen_model, track_history, frame)
+def process_frame(frame):
+   result_frame, _ = predict_and_detect(chosen_model, frame)
    return result_frame
 
 
@@ -107,10 +98,6 @@ def process_frame(track_history, frame):
 def main():
    skip_frames = 2  # Number of frames to skip before processing the next one
    frame_count = 0
-
-   # Store the track history
-   track_history = defaultdict(lambda: [])
-
    with concurrent.futures.ThreadPoolExecutor() as executor:
        while True:
            ret, frame = cap.read()
@@ -121,7 +108,7 @@ def main():
                continue  # Skip this frame
 
            # Submit the frame for processing
-           future = executor.submit(process_frame, track_history, frame)
+           future = executor.submit(process_frame, frame)
            result_frame = future.result()
 
            # Display the processed frame
