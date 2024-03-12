6adbef6e242bd813a1f5c390c7a0b67ee9d4fe96
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2024-03-13 01:18:16 +0800

    Update multi_track.py

--- multi_track.py
+++ multi_track.py
@@ -1,5 +1,6 @@
 import threading
 import subprocess
+import argparse
 
 def run_command(command):
     """
@@ -10,32 +11,33 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
 
-def main():
+def main(opt):
     # read source.streams 
     with open("source.streams","r") as f:
         sources = f.readlines()
     sources = [item.strip("\n") for item in sources]
 
     # read geofencing.streams 
-    with open("geofencing.streams","r") as f:
-        geofencings = f.readlines()
-    geofencings = [item.strip("\n") for item in geofencings]    
-
-    # assertion
-    if len(geofencings) != 0:
+    if opt.geofencing:
+        with open("geofencing.streams","r") as f:
+            geofencings = f.readlines()
+        geofencings = [item.strip("\n") for item in geofencings]    
+    
         # make sure the geofencing ROI are correct
         assert len(sources) == len(geofencings), 'Please provide the corresponding geofencing ROI for each video streaming source'
+    else:
+        geofencings = [None] * len(sources)
 
-        # try if drive utility function is working
-        # before multithreading
-        try:
-            from counter import Counter
-            counter = Counter(0,0,0,0,0)   # init a dummy counter
-            counter.drive_handler.post()   # try posting smtg to see if API working
-            del counter                    # delete the dummy counter
-        except Exception as e:
-            print(e)
-            input('Please type anything to continue')
+    # try if drive utility function is working
+    # before multithreading
+    try:
+        from counter import Counter
+        counter = Counter(0,0,0,0,0)   # init a dummy counter
+        counter.drive_handler.post()   # try posting smtg to see if API working
+        del counter                    # delete the dummy counter
+    except Exception as e:
+        print(e)
+        input('Please type anything to continue')
 
     # define list to store all threads
     threads  = []
@@ -45,19 +47,24 @@ def main():
         # command
         try:
             source = int(source)
-            command = f'python3 single_track.py --camera {source} --stream-idx {stream_idx} --roi-xyxy "{geofencing}"'
+            command = f'python3 single_track.py --camera {source} --stream-idx {stream_idx}'
         except:
             if source.endswith('.mp4'):
-                command = f'python3 single_track.py --video-file "{source}" --stream-idx {stream_idx} --roi-xyxy "{geofencing}"'
+                command = f'python3 single_track.py --video-file "{source}" --stream-idx {stream_idx}'
             elif source.startswith('rtsp'):
-                command = f'python3 single_track.py --rtsp "{source}" --stream-idx {stream_idx} --roi-xyxy "{geofencing}"'
+                command = f'python3 single_track.py --rtsp "{source}" --stream-idx {stream_idx}'
             elif source.startswith('http://www.youtube.com'):
-                command = f'python3 single_track.py --youtube "{source}" --stream-idx {stream_idx} --roi-xyxy "{geofencing}"'
+                command = f'python3 single_track.py --youtube "{source}" --stream-idx {stream_idx}'
             elif source.rstrip() == '':
                 print('please prevent empty lines in source.streams')
                 continue
             else:
                 raise NotImplementedError
+
+        # if geofencing
+        if opt.geofencing:
+            command += f' --roi-xyxy "{geofencing}"'
+        
         # thread
         thread = threading.Thread(target=run_command, args=(command,))
         threads.append(thread)        
@@ -71,5 +78,9 @@ def main():
         thread.join()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--geofencing', action='store_true',
+                        help='if flagged, activate geofencing')
+    opt = parser.parse_args()
 
+    main(opt)
